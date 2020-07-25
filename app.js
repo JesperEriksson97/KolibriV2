d53415d73f763ffc2d183154324888e86d4588e4
Jesper Eriksson <je223gs@student.lnu.se>
2020-07-25 16:51:01 +0200

    adding socketio

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
+
 const bodyParser = require("body-parser");
+
+const socketio = require('socket.io')
+const mongoose = require('mongoose')
 
 const http = require("http");
 const path = require("path");
 
 const hbs = require("express-hbs");
 
+const server = http.createServer(app);
 const PORT = 3000 || process.env.PORT;
+
+const io = socketio(server)
 
 // Body Parser
 app.use(bodyParser.json())
@@ -25,6 +32,7 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 
+// HBS View Engine Setup
 app.engine(
   "hbs",
   hbs.express4({
@@ -32,7 +40,16 @@ app.engine(
   })
 );
 
-const server = http.createServer(app);
+// Socket 
+io.on('connection', (socket) => {
+  console.log('Connection established with socket id: ', socket.id)
+  console.log(socket)
+
+  socket.emit('message', 'Hello World')
+})
+
+
+
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
